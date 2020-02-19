d2d41d630bd82f13b8fdec4b2d2903b850a8325e
Karl Gironda <kwak@Karls-MacBook-Pro.local>
2020-02-18 21:34:54 -0500

    styling round 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
-import Adopt from "./Components/Adopt/Adopt";
+import NavbarDisplay from "./Components/Navbar/NavbarDisplay";
+// import Adopt from "./Components/Adopt/Adopt";
+// import Paper from "@material-ui/core/Paper";
+// import Button from "@material-ui/core/Button"
+
+
+
 
 function App() {
   return (
-    <div>
-      <Adopt />
+    <div className="maindiv">
+    
+      <NavbarDisplay />
+      <h3 className="mission">Tired of seeing a depressing scene as you mindlessly scroll through your social media?</h3>
+      <h1 className="mission">Don't!</h1> 
+      <h3 className="mission">Stay happy and keep your heart full at Petazoa! </h3>
+      
     </div>
   );
 }
