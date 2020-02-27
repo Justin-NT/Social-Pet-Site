a796e6dd51896ac5f71f22b9ed00669038f642f2
Dylan Morozowski <dymacski@gmail.com>
2020-02-27 10:19:17 -0500

    some changes to personal branch

---
+++ Client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Adopt from "./Components/Adopt/Adopt";
+
+function App() {
+  return (
+    <div>
+      <Adopt />
+    </div>
+  );
+}
+
+export default App;
